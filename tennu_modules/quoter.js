7483708fccd187d6d07a6374630d90a41c6e740f
Marc Talbot <mtalbot@seilevel.com>
2014-01-11 15:31:07 -0600

    Update to use promises

--- tennu_modules/quoter.js
+++ tennu_modules/quoter.js
@@ -2,56 +2,68 @@
 var QuoteModule = {
     init: function (client, imports) {
         var cheerio = require('cheerio');
-        var request = require('request');
+        var Promise = require('bluebird');
+        Promise.longStackTraces();
+        var request = Promise.promisify(require('request'));
         var Random = require('hrandom');
-        var random = new Random(Math.floor(Math.random() * 17) + 46);
-        var getUrl = function(search, command) {
-            request.get('http://www.google.com/search?as_q=' + search + '&as_sitesearch=brainyquote.com', 
-                function(error, response, body) {
-                if (!error && response && response.statusCode === 200) {
-                    console.log("Got data back...");
-                    $ = cheerio.load(body);
-                    test = $('h3.r a').first()[0];
-                    if (test) { 
-                        url = test.attribs.href;
-                        url = url.substring(7, url.indexOf(".html") + 5);
-                        client.say(command.channel, url);
-                        var getQuotes = function(url) { 
-                            request.get(url, function(error, response, body) {
-                                if (!error && response && response.statusCode === 200) {
-                                    console.log("Searched for quotes");
-                                    $ = cheerio.load(body);
-                                    quotes = $('span.bqQuoteLink a');
-                                    if (quotes[0] && quotes[0].children) {
-                                        client.say(command.channel, '"' + random.nextElement(quotes).children[0].data + '"');
-                                    }
-                                } else {
-                                    console.log("Something went wrong.");
-                                    console.log(" Error = " + error);
-                                    console.log(" Response = " + response);
-                                    if (response) {
-                                        console.log(" Status code = " + response.statusCode);
-                                    }
-                                }
-                            });
-                        };
-                        getQuotes(url);
-                    } else {
-                        client.say(command.channel, "Can't find any quotes, sorry");
-                    }
-
-                } else {
-                    console.log("Something went wrong " + err);
-                }
-            });
-        };
+        var random = new Random();
 
         return {
             handlers: {
                 '!quote': function (command) {
+                    var getGoogleUrl = function(searchTerm) {
+                        return request('http://www.google.com/search?as_q=' + searchTerm + 
+                            '&as_sitesearch=brainyquote.com')
+                        .spread(function (response, body) {
+                                $ = cheerio.load(body);
+                                var googleResult = $('h3.r a').first()[0];
+                                if (googleResult) { 
+                                    var url = googleResult.attribs.href
+                                    url = url.substring(7, url.indexOf(".html") + 5);
+                                    return url;
+                                } else { 
+                                    throw "Couldn't find any quotes";
+                                }
+                        });
+
+                    };
+
+                    var getQuoteResults = function(url) {
+                        client.say(command.channel, url);
+                        return request(url)
+                        .spread(function(response, body) {
+                            if (response && response.statusCode === 200) {
+                                $ = cheerio.load(body);
+                                var quotes = $('span.bqQuoteLink a'); 
+                                if (quotes[0] && quotes[0].children) {
+                                   return quotes;
+                                } else {
+                                    var quote = $('.bq_fq_lrg p')[0].children[0].data;
+                                    console.log("Hail Mary: " + quote);
+                                    return [{ children: [ { data: quote } ] }];
+                                }
+                            } else {
+                                throw "Unable to parse brainy quote response";
+                            }
+                        });
+                    };
+
+                    var pickResults = function(results) {
+                        if (results[0] && results[0].children) {
+                            client.say(command.channel, '"' + random.nextElement(results).children[0].data + '"');
+                        } else {
+                            throw "Couldn't handle result";
+                        }
+                    }
                     command.args.shift
                     if (command.args.length > 0) {
-                        getUrl(command.args.join('+'), command);
+                        getGoogleUrl(command.args.join('+'))
+                        .then(getQuoteResults)
+                        .then(pickResults)
+                        .catch(function(err) {
+                            console.log(err);
+                            client.say(command.channel, "Sorry!");
+                        });
                     }
                 }
             },
